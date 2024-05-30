4aac42ad06ee54d678a714c5615c588d55a727b3
Mscourtneybryant <87495361+Mscourtneybryant@users.noreply.github.com>
2024-05-30 13:39:33 -0500

    created some divs, created an element using JS, created text using JS, grabbed elements by their ID, added some background color using JS and added event listeners

---
+++ app.js
@@ -0,0 +1,62 @@
+//set the background color of the reservation header div
+const rsvp = document.getElementById("rsvpHeader");
+rsvp.style.background = "#1a0000";
+
+//set the background color of the reservation list
+const rsvpNames = document.getElementById("rsvpList");
+rsvpNames.style.background = "gray";
+
+//set the background color for the second div with reservation info
+const rsvpInfo = document.getElementById("secondDiv");
+rsvpInfo.style.background = "gray";
+
+
+//create an h1 for the reservation div
+let rsvpH1 = document.createElement('h1');
+rsvpH1.textContent = `Reservations for ${new Date()}`;
+let rsvpDiv = document.getElementById("rsvpHeader");
+//append the element to the div
+rsvpDiv.appendChild(rsvpH1);
+
+//cache an element using querySelector
+const myNav = document.getElementById("navBar");
+myNav.style.color = "#cc0000";
+
+//items to be added to the list of reservations
+const rsvpPeople = ["Amy", "Michelle", "Jamie"]
+
+const rdocfrag = document.createDocumentFragment();
+for (let x in rsvpPeople){
+    const li = document.createElement('li');
+    li.textContent = rsvpPeople[x];
+    rdocfrag.appendChild(li);
+}
+
+//people to add to rsvp List
+document.getElementById('myRsvpList').appendChild(rdocfrag);
+
+//Modify the text of two elements upon user interaction
+let rsvpDisplayInfo = document.getElementById("clickable1");
+let rsvpDisplayInfo2 = document.getElementById("clickable2");
+//dates to be shwon when clicking on a name with a reservation
+let rsvpdate1 = 'May 30, 2024 at 8:00 PM';
+let rsvpdate2 = 'May 30, 2024 at 9:00 PM';
+let rsvpdate3 = 'May 31, 2024 at 4:00 PM';
+
+rsvpDisplayInfo.addEventListener("click", displayInfo);
+rsvpDisplayInfo2.addEventListener("click", displayInfo2);
+
+function displayInfo() {
+    document.getElementById("clickable1").textContent = `Reservation Date: ${rsvpdate1}`;
+}
+
+function displayInfo2() {
+    document.getElementById("clickable2").textContent = `Reservation Date: ${rsvpdate2}`;
+}
+
+//let the mouse become a pointer when clicking on a name from the reservation list
+document.getElementById("myRsvpList").style.cursor = "pointer";
+
+
+//creati
+
